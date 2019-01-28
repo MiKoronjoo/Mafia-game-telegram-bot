7264fd0517803277920d8c1a7977fcf9b5da9225
Hassan <hassan.abbp@gmail.com>
2019-01-28 14:33:14 +0330

    Add role_type to roles

--- game/roles.py
+++ game/roles.py
@@ -1,17 +1,32 @@
+import enum
+
+
+class RoleType(enum.Enum):
+    WHITE = 'WHITE'
+    GREY = 'GREY'
+    BLACK = 'BLACK'
+
+
 class Role:
     pass
 
 
 class Doctor(Role):
+    def __init__(self):
+        self.role_type = RoleType.WHITE
+
     def save(self):
         pass
 
 
 class Citizen(Role):
-    pass
+    def __init__(self):
+        self.role_type = RoleType.WHITE
 
 
 class Mafia(Role):
+    def __init__(self):
+        self.role_type = RoleType.BLACK
     def kill(self):
         pass
 
@@ -21,24 +36,33 @@ class Don(Mafia):
 
 
 class Joker(Role):
-    pass
+    def __init__(self):
+        self.role_type = RoleType.GREY
 
 
 class Natasha(Role):
+    def __init__(self):
+        self.role_type = RoleType.GREY
     def mute(self):
         pass
 
 
 class Priest(Role):
+    def __init__(self):
+        self.role_type = RoleType.WHITE
     def unmute(self):
         pass
 
 
 class Thief(Role):
+    def __init__(self):
+        self.role_type = RoleType.GREY
     def steal(self):
         pass
 
 
 class Sniper(Role):
+    def __init__(self):
+        self.role_type = RoleType.WHITE
     def shoot(self):
         pass
